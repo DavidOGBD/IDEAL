1523f3081f93856d522af4b4cd72d96e40581552
David Ogbada <davidogbada020@gmail.com>
2026-06-14 19:11:28 +0100

    Completed food logger

--- food_logger.py
+++ food_logger.py
@@ -1,1 +1,33 @@
+import json
+import os
 
+
+class FoodLogger:
+
+    def save(self, product):
+
+        data = {
+            "name": product.name,
+            "barcode": product.barcode
+        }
+
+        filename = "food_log.json"
+
+        if not os.path.exists(filename):
+
+            with open(filename, "w") as file:
+                json.dump([], file)
+
+        with open(filename, "r") as file:
+            logs = json.load(file)
+
+        logs.append(data)
+
+        with open(filename, "w") as file:
+            json.dump(
+                logs,
+                file,
+                indent=4
+            )
+
+        print("Food Logged Successfully")
